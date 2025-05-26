81ddcd171b1b58eff07fdf3c73ccfe341b89d54c
Muhammad Syahroni <73042737+Syahroni12@users.noreply.github.com>
2025-05-27 03:32:16 +0700

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -19,8 +19,8 @@ php artisan migrate --force
 # php artisan db:seed --force
 
 # Permissions
-chown -R www-data:www-data /var/www/storage /var/www/bootstrap/cache
-chmod -R 775 /var/www/storage /var/www/bootstrap/cache
+chown -R www-data:www-data storage bootstrap/cache
+chmod -R 775 storage bootstrap/cache
 
 php artisan config:clear
 php artisan cache:clear
