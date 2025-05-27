5a32a409bb11153a9625f3c3b0c381c4406628bf
achbayhq <achmadbayhaqiprog@gmail.com>
2025-05-27 22:38:01 +0700

    add dashboardcustomertest.php untuk testing rev3

--- tests/Feature/DashboardCustomerTest.php
+++ tests/Feature/DashboardCustomerTest.php
@@ -18,10 +18,10 @@ class DashboardCustomerTest extends TestCase
     protected function setUp(): void
     {
         parent::setUp();
-        
+
         // Jalankan migrasi
         $this->artisan('migrate:fresh');
-        
+
         // Buat customer
         $this->customer = User::factory()->customer()->create();
     }
@@ -30,11 +30,11 @@ class DashboardCustomerTest extends TestCase
     public function customer_can_access_dashboard()
     {
         $response = $this->actingAs($this->customer)
-                        ->get('/customer/dashboard/home');
-        
+            ->get('/customer/dashboard/home');
+
         $response->assertStatus(200)
-                ->assertViewIs('customer.dashboard.home') // Sesuaikan dengan view Anda
-                ->assertSeeText('Hi, Selamat Pagi');
+            ->assertViewIs('customer.dashboard.home') // Sesuaikan dengan view Anda
+            ->assertSeeText('Hi, Selamat Pagi');
     }
 
     /** @test */
@@ -42,10 +42,10 @@ class DashboardCustomerTest extends TestCase
     {
         // Buat produk milik customer ini
         $product = Produk::factory()
-                    ->create(['id_user' => $this->customer->id]);
+            ->create(['id_user' => $this->customer->id]);
 
         $response = $this->actingAs($this->customer)
-                        ->get('/customer/dashboard/home');
+            ->get('/customer/dashboard/home');
 
         $response->assertSeeText($product->nama);
     }
@@ -55,20 +55,24 @@ class DashboardCustomerTest extends TestCase
     {
         // Buat penyewaan
         $rental = Penyewaan::factory()
-                  ->create(['id_user' => $this->customer->id]);
+            ->create(['id_user' => $this->customer->id]);
 
         // Buat detail penyewaan
         $product = Produk::factory()
-                    ->create(['id_user' => $this->customer->id]);
-                    
+            ->create(['id_user' => $this->customer->id]);
+
         DetailPenyewaan::factory()
             ->create([
                 'id_penyewaan' => $rental->id,
-                'id_produk' => $product->id
+                'id_produk' => $product->id,
+                'warna_produk'=> 'merah',
+                'ukuran'=> 'L',
+                'qty'=> '5',
+                'subtotal'=> '400000',
             ]);
 
         $response = $this->actingAs($this->customer)
-                        ->get('/customer/dashboard/home');
+            ->get('/customer/dashboard/home');
 
         $response->assertSeeText($rental->nama_penyewa);
     }
